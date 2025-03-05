4ebad62dc304635341c06a663881d0149ef5e24b
Fabrício José Barrozo <ueredeveloper@gmail.com>
2025-03-05 13:19:17 -0300

    feat: jframe com java fx

--- src/main/java/application/jframe/Main.java
+++ src/main/java/application/jframe/Main.java
@@ -15,7 +15,7 @@ public class Main extends JFrame {
     private static final long serialVersionUID = 7052387842413089905L;
 
     JLayeredPane layeredPane;
-    BrowserFXPanel browserFxPanel;
+    BrowserFXPanel mapFxPanelPanel;
     ContentFXPanel contentFxPanel;
 
     public Main() {
@@ -30,41 +30,27 @@ public class Main extends JFrame {
         layeredPane.setPreferredSize(new Dimension(1920, 1080));
 
         // Create Browser and JavaFX Panels
-        browserFxPanel = new BrowserFXPanel();
+        mapFxPanelPanel = new BrowserFXPanel();
         contentFxPanel = new ContentFXPanel();
 
         // Set bounds for proper layering
-        browserFxPanel.setBounds(0, 0, 1920, 1080);
+        mapFxPanelPanel.setBounds(0, 0, 1920, 1080);
         contentFxPanel.setBounds(200, 100, 1920, 1080); // Position JavaFX panel on top
 
-        Component browserFx = browserFxPanel.getPanel();
+        Component mapFxPanel = mapFxPanelPanel.getPanel();
         // Add to layeredPane
-       // layeredPane.add(browserFx, Integer.valueOf(0)); // Back layer
-      layeredPane.add(contentFxPanel, Integer.valueOf(0)); // Front layer
+       layeredPane.add(mapFxPanel, Integer.valueOf(0)); // Back layer
+       layeredPane.add(contentFxPanel, Integer.valueOf(1)); // Front layer
         
         // Force JavaFX panel to be on top after any layout change
         layeredPane.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
-                int zOrderMap = layeredPane.getComponentZOrder(browserFx);
+                int zOrderMap = layeredPane.getComponentZOrder(mapFxPanel);
                 int zOrderContent = layeredPane.getComponentZOrder(contentFxPanel);
                 System.out.println("component resized - map " + zOrderMap + " content " + zOrderContent);
                 
-                if (zOrderMap==-1) {
-                	layeredPane.add(browserFx, Integer.valueOf(0));
-                	layeredPane.add(contentFxPanel, Integer.valueOf(1));
-                	contentFxPanel.repaint();
-                } else if (zOrderMap==1) {
-                	layeredPane.add(browserFx, Integer.valueOf(0));
-                	layeredPane.add(contentFxPanel, Integer.valueOf(1));
-                	
-                	browserFx.repaint();
-                	 
-                } else if (zOrderMap == 0) {
-                	browserFx.repaint();
-                	contentFxPanel.repaint();                	
-                }
-                 
+               
 
          
             }
